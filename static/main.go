af0ba851f9306ff79e58c687c32c81576dd4a1ec
atanda nafiu <atandanafiu21@gmail.com>
2023-01-20 23:56:18 +0100

    server up

--- static/main.go
+++ static/main.go
@@ -36,7 +36,7 @@ func main() {
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
-	fmt.Println("Starting server at port 8080")
+	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal()
 	}
